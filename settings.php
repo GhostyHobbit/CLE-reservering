e8d2d3b8c130982acaa392f2143f4d0ced104582
nestor <neskoh27@hotmail.com>
2024-01-11 14:20:09 +0100

    settings added (not finished!)

---
+++ settings.php
@@ -0,0 +1,40 @@
+<?php
+session_start();
+
+//establishes connection to the database
+/** @var mysqli $db */
+require_once 'database.php';
+$db = mysqli_connect($host, $user, $password, $database)
+or die("Error: " . mysqli_connect_error());
+
+//gets the users data from the database
+$query = "SELECT * FROM users WHERE email = '$_SESSION'['email']";
+$result = mysqli_query($db, $query);
+//puts the data in an array
+$user = mysqli_fetch_assoc($result);
+?>
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+<header>
+    <h1>Settings</h1>
+</header>
+<main>
+    <h2><?= $user['first_name']?> <?= $user['first_name']?></h2>
+    <h3><?= $user['email']?></h3>
+    <h3><?= $user['adress']?></h3>
+    <h3><?= $user['postcode']?></h3>
+    <h3><?= $user['city']?></h3>
+</main>
+<footer>
+
+</footer>
+</body>
+</html>
