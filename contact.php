d9e8f7ffd5994fd657e8a34c38a799f86ac9d73d
Lucas <1066431@hr.nl>
2024-01-12 11:49:09 +0100

    contact css first version

--- contact.php
+++ contact.php
@@ -9,6 +9,7 @@
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link rel="stylesheet" href="css/global.css">
+    <link rel="stylesheet" href="css/contact.css">
     <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Almarai&family=Annie+Use+Your+Telescope&display=swap">
     <title>Contact Pagina</title>
 </head>
@@ -26,12 +27,16 @@
         </div>
     </nav>
     <main>
+        <img src="images/wool.png" alt="wol-collage">
         <div>
-
-        </div>
-        <div>
-            <h1></h1>
-            <p></p>
+            <h1>Over Wolhoop</h1>
+            <p>lorum ipsum blabla ur mom</p>
+            <h2>Contact</h2>
+            <div class="links">
+                <a href="https://www.instagram.com/dewolhoopspinning/">Instagram: @dewolhoopspinning</a>
+                <a href="https://www.facebook.com/groups/3217490328265360/media" class="facebook">Facebook: De Wolhoop</a>
+            </div>
+            <p>evt email etc</p>
         </div>
     </main>
     <footer>
