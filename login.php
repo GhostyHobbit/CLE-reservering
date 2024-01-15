a2189b2c0e1d92dbf95e491a70587d2accde5503
Lucas <1066431@hr.nl>
2024-01-15 09:57:36 +0100

    login header n footer

--- login.php
+++ login.php
@@ -78,6 +78,8 @@ if (isset($_POST['submit'])) {
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link rel="stylesheet" href="css/global.css">
+    <link href="https://fonts.googleapis.com/css2?family=Almarai&family=Annie+Use+Your+Telescope&display=swap"
+          rel="stylesheet">
     <title>Log in</title>
 </head>
 <body>
