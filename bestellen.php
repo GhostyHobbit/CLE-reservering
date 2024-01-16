a650d08509702b18b9621ff5bfe22ee6018c4456
Lucas <1066431@hr.nl>
2024-01-16 14:28:49 +0100

    begin bestelling css

--- bestellen.php
+++ bestellen.php
@@ -13,7 +13,7 @@ if (isset($_POST['submit'])) {
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="css/bestellen.css">
     <link rel="stylesheet" href="css/global.css">
     <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Almarai&family=Annie+Use+Your+Telescope&display=swap">
     <title>Bestellen</title>
@@ -34,46 +34,73 @@ if (isset($_POST['submit'])) {
     </div>
 </nav>
 
-<!--begin formulier-->
-<form action="bestellenGegevens.php" method="get">
-    <label for="colour_amount1">1 Kleur</label>
-    <input type="radio" id="colour_amount1" name="colour_amount" value="1" >
-    <label for="colour_amount2">2 Kleuren</label>
-    <input type="radio" id="colour_amount2" name="colour_amount" value="2">
-    <label for="colour_amount3">3 Kleuren</label>
-    <input type="radio" id="colour_amount3" name="colour_amount" value="3">
-    <br>
+<main>
+    <h1>Bestellen: Kies uw kleuren</h1>
+    <!--begin formulier-->
+    <form action="bestellenGegevens.php" method="get">
 
-    <label for="rope_length">Lengte Wol</label>
-    <input list="rope_length" name="rope_length">
-        <datalist id="rope_length">
-            <option  value="50cm"></option>
-            <option  value="100cm"></option>
-            <option  value="150cm"></option>
-            <option  value="200cm"></option>
-        </datalist>
-    <br>
+        <div class="rowFlex">
+            <div>
+                <div class="img">
+                    <img src="images/1kleur.jpg" alt="één-kleur">
+                </div>
+                <label for="colour_amount1">1 Kleur</label>
+                <input type="radio" id="colour_amount1" name="colour_amount" value="1" >
+                <span class="checkmark"></span>
+            </div>
+            <div>
+                <div class="img">
+                    <img src="images/2kleur.jpg" alt="twee-kleuren">
+                </div>
+                <label for="colour_amount2">2 Kleuren</label>
+                <input type="radio" id="colour_amount2" name="colour_amount" value="2">
+                <span class="checkmark"></span>
+            </div>
+            <div>
+                <div class="img">
+                    <img src="images/3kleur.jpg" alt="drie-kleuren">
+                </div>
+                <label for="colour_amount3">3 Kleuren</label>
+                <input type="radio" id="colour_amount3" name="colour_amount" value="3">
+                <span class="checkmark"></span>
+            </div>
+        </div>
 
-    <label for="colours">Kleuren</label>
-    <textarea id="colours" name="colours"></textarea>
-    <br>
+        <div class="fields">
+            <div class="stack">
+                <label for="rope_length">Lengte Wol</label>
+                <input list="rope_length" name="rope_length">
+                    <datalist id="rope_length">
+                        <option  value="50cm"></option>
+                        <option  value="100cm"></option>
+                        <option  value="150cm"></option>
+                        <option  value="200cm"></option>
+                    </datalist>
+            </div>
+            <div class="stack">
+                <label for="colours">Kleuren</label>
+                <textarea id="colours" name="colours"></textarea>
+            </div>
+            <div class="stack">
+                <label for="comments">Opmerkingen</label>
+                <textarea id="comments" name="comments"></textarea>
+            </div>
+            <div class="stack">
+                <label for="rope_amount">Hoeveel bolletjes?</label>
+                <input list="rope_amount" name="rope_amount">
+                <datalist id="rope_amount">
+                    <option  value="1"></option>
+                    <option  value="2"></option>
+                    <option  value="3"></option>
+                    <option  value="4"></option>
+                </datalist>
+            </div>
 
-    <label for="comments">Opmerkingen</label>
-    <textarea id="comments" name="comments"></textarea>
-    <br>
 
-    <input list="rope_amount" name="rope_amount">
-    <datalist id="rope_amount">
-        <option  value="1"></option>
-        <option  value="2"></option>
-        <option  value="3"></option>
-        <option  value="4"></option>
-    </datalist>
-    <br>
-
-    <input type="submit" id="submit" name="submit" >
-
-</form>
+            <input type="submit" id="submit" name="submit" >
+        </div>
+    </form>
+</main>
 <footer>
     <img src="images/Logo-reserveringsysteem.png" alt="wolhoop-logo">
     <div>
