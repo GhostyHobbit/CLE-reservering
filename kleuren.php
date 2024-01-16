037cf148ba6fab5b4ed9227a48492eda2a726dc1
RoshanMomand <1028086@hr.nl>
2024-01-16 10:57:19 +0100

    Kleuren

--- kleuren.php
+++ kleuren.php
@@ -1,5 +1,6 @@
 <?php
 
+
 ?>
 <!doctype html>
 <html lang="en">
@@ -8,7 +9,7 @@
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="css/kleuren.css">
     <link rel="stylesheet" href="css/global.css">
     <link href="https://fonts.googleapis.com/css2?family=Almarai&family=Annie+Use+Your+Telescope&display=swap"
           rel="stylesheet">
@@ -30,11 +31,13 @@
     </div>
 </nav>
 <main>
-    <section id="best-seller">
+    <section id="kleuren-bestseller">
         <h1>Best Verkocht</h1>
+        <div class="bestseller">
         <img src="images/Placeholder.png " alt=""> <!--  -->
         <img src="images/Placeholder.png " alt="">
         <img src="images/Placeholder.png " alt="">
+        </div>
     </section>
     <section id="season-colors">
         <h1>Seizoen Combinaties</h1>
