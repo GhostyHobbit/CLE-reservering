97b7b7666c50a9f1dfb531a4ab22000a3e7fca3b
Lucas <1066431@hr.nl>
2024-01-22 12:36:48 +0100

    details order page

--- orders.php
+++ orders.php
@@ -33,7 +33,7 @@ mysqli_close($db);
         <a href="index.php">Home</a>
         <a href="blog.php">Blog</a>
         <a href="kleuren.php">Kleuren</a>
-        <a href="bestellen.php">Bestellen</a>
+        <a href="orders.php" class="location">Bestellingen</a>
         <a href="contact.php">Over Wolhoop</a>
     </div>
     <div class="login">
